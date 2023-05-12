cb03d152563028cbc1efbfeada6f255ead9a5e01
DS <mc_sd@bk.ru>
2023-05-12 18:00:06 -0300

    JSON

---
+++ src/main/java/ru/netology/stats/FormDate.java
@@ -0,0 +1,9 @@
+package ru.netology.stats;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+
+
+}
---
+++ src/main/java/ru/netology/stats/Post.java
@@ -0,0 +1,18 @@
+package ru.netology.stats;
+
+public class Post {
+    FormDate birthday= new FormDate();
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String telefone;
+    public String surname;
+    boolean subscription = true;
+    public Post (){
+        subscription=true;
+    }
+
+
+
+}
+
